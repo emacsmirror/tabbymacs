7137a61c4480edfb54c6badd0ffb10d707fda953
Bastillan <kedzierski.jedrzej@gmail.com>
2025-08-21 15:15:09 +0200

    feat: sending initialized request with the same error as in eglot

--- tabbymacs.el
+++ tabbymacs.el
@@ -3,46 +3,56 @@
 (require 'cl-lib)
 
 (defvar tabbymacs--proc nil)
-(defvar tabbymacs--connection nil)
-(defvar tabbymacs--request-id 0)
+(defvar tabbymacs--connection nil
+  "The JSONRPC connection to tabby-agent")
 
-(defun tabbymacs--initialize()
-  "Send the LSP initialize request to the tabby-agent."
-  (jsonrpc-request
-   tabbymacs--connection
-   "initialize"
-   `((processId . ,(emacs-pid))
-	 (rootUri . ,(concat "file://" default-directory))
-	 (capabilities . ((textDocument . ((synchronization . ((didSave . t)))))))
-	 (clientInfo . ((name . "tabbymacs")
-				   (version . "0.1"))))))
-
-(defun tabbymacs--initialized ()
-  (jsonrpc-notify tabbymacs--connection "initialized" '()))
-
-(defun tabbymacs-start ()
-  "Start Tabbymacs LSP client."
-  (interactive)
-  (setq tabbymacs--proc
-		(make-process
-		 :name "tabby-agent"
-		 :command '("tabby-agent" "--lsp" "--stdio")
-		 :connection-type 'pipe
-		 :coding 'utf-8
-		 :stderr "*tabby-agent stderr*"
-		 :noquery t))
+(defun tabbymacs--make-connection()
+  "Start a connection to tabby-agent via LSP (stdio)."
   (setq tabbymacs--connection
-		(jsonrpc-process-connection
+		(make-instance
+		 'jsonrpc-process-connection
 		 :name "tabby-agent"
-		 :process tabbymacs--proc
-		 :request-dispatcher #'jsonrpc-default-request-dispatcher
-		 :notification-dispatcher #'jsonrpc-default-notification-dispatcher
-		 :on-shutdown (lambda (_conn) (message "Tabby server shut down"))))
-  ;; Send initialize request after starting
-  (message "Sending initialize request...")
-  (tabbymacs--initialize)
-  (tabbymcas--initialized))
+		 :process (lambda ()
+					(make-process
+					 :name "tabby-agent"
+					 :command '("tabby-agent" "--stdio")
+					 :connection-type 'pipe
+					 :coding 'utf-8-emacs-unix
+					 :stderr "*tabby-agent-stderr*"))
+		 :request-dispatcher
+		 (lambda (_conn method params)
+		   (message "[tabby-agent] Request from server: %s %S" method params)
+		   nil)
+		 :notification-dispatcher
+		 (lambda (_conn method params)
+		   (message "[tabby-agent] Notification: %s %S" method params)))))
 
+(defun tabbymacs-connect ()
+  "Connect to tabby-agent if not already connected."
+  (interactive)
+  (unless (and tabbymacs--connection
+			   (jsonrpc-running-p tabbymacs--connection))
+	(tabbymacs--make-connection)
+	(let* ((root (expand-file-name default-directory))
+		   (init-params `(:processId ,(emacs-pid)
+									 :rootUri ,(concat "file://" root)
+									 :capabilities (:textDocument (:completion (:completionItem
+																				(:snippetSupport t)))))))
+	  (jsonrpc-request tabbymacs--connection
+					   :initialize init-params))
+	(jsonrpc-notify tabbymacs--connection :initialized '(:dummy t))
+	(message "Started tabby-agent connection.")))
+
+(defun tabbymacs-disconnect ()
+  "Shutdown the connection to tabby-agent."
+  (interactive)
+  (when (and tabbymacs--connection
+			 (jsonrpc-running-p tabbymacs--connection))
+	(jsonrpc-request tabbymacs--connection :shutdown nil)
+	(jsonrpc-notify tabbymacs--connection :exit nil)
+	(jsonrpc-shutdown tabbymacs--connection)
+	(setq tabbymacs--connection nil)
+	(message "Stopped tabby-agent connection.")))
 
 (provide 'tabbymacs)
 
