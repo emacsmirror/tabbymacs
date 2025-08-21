047b587e1a6e02b4c78c0d1f267066f55dd41e67
Bastillan <kedzierski.jedrzej@gmail.com>
2025-08-21 17:28:13 +0200

    feat: working initialize request

--- tabbymacs.el
+++ tabbymacs.el
@@ -1,10 +1,17 @@
 (require 'json)
 (require 'jsonrpc)
 (require 'cl-lib)
+(require 'url-util)
 
 (defvar tabbymacs--proc nil)
 (defvar tabbymacs--connection nil
   "The JSONRPC connection to tabby-agent")
+
+(defun tabbymacs--path-to-uri (path)
+  "Convert PATH to a proper file:// URI with escaping."
+  (concat "file://"
+		  (url-hexify-string (expand-file-name (file-local-name path))
+							 url-path-allowed-chars)))
 
 (defun tabbymacs--make-connection()
   "Start a connection to tabby-agent via LSP (stdio)."
@@ -34,17 +41,27 @@
 			   (jsonrpc-running-p tabbymacs--connection))
 	(tabbymacs--make-connection)
 	(let* ((root (expand-file-name default-directory))
-		   (init-params `(:processId ,(emacs-pid)
-									 :rootUri ,(concat "file://" root)
-									 :capabilities (:textDocument (:completion (:completionItem
-																				(:snippetSupport t)))))))
+		   (uri (tabbymacs--path-to-uri root))
+		   (init-params
+			`(:processId ,(emacs-pid)
+						 :clientInfo (:name "emacs" :version ,(emacs-version))
+						 :rootPath ,(file-local-name root)
+						 :rootUri ,uri
+						 :workspaceFolders [(:uri ,uri
+												  :name ,(file-name-nondirectory
+														  (directory-file-name root)))]
+						 :capabilities
+						 (:textDocument
+						  (:completion
+						   (:completionItem
+							(:snippetSupport t)))))))
 	  (jsonrpc-request tabbymacs--connection
 					   :initialize init-params))
 	(jsonrpc-notify tabbymacs--connection :initialized '(:dummy t))
 	(message "Started tabby-agent connection.")))
 
 (defun tabbymacs-disconnect ()
-  "Shutdown the connection to tabby-agent."
+  "Shutdown the connection to tabby-agent." ;
   (interactive)
   (when (and tabbymacs--connection
 			 (jsonrpc-running-p tabbymacs--connection))
